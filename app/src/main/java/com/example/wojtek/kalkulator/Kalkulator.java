da10b0961cf074fc53ba5bc7a80f47a2d17606a0
Wojtek <nwojtek@gmail.com>
2018-10-13 17:25:32 +0200

    Prawie skonczony kalklutor

--- app/src/main/java/com/example/wojtek/kalkulator/Kalkulator.java
+++ app/src/main/java/com/example/wojtek/kalkulator/Kalkulator.java
@@ -1,13 +1,31 @@
 package com.example.wojtek.kalkulator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Kalkulator extends AppCompatActivity {
-
+    //public static final String EXTRA_MESSAGE = "com.example.wojtek.kalkultor";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalkulator);
+    }
+
+    public void oMnie(View view){
+        Intent intent = new Intent(this,oMnieAct.class);
+        startActivity(intent);
+    }
+    public void egzit(View view){
+        finish();
+        System.exit(0);
+    }
+    public void basicCalc(View view){
+        Intent intent = new Intent(this,basicCalc.class);
+        startActivity(intent);
+    }
+    public void advCalc(View view){
+
     }
 }
