6e8e3d6f7344c7a5643d260e61cad5409e3c4237
Wojtek Niedzielski <nwojtek@gmail.com>
2019-01-16 22:49:51 +0100

    numerki gotowe

--- app/src/main/java/com/example/wojtek/kalkulator/advCalc.java
+++ app/src/main/java/com/example/wojtek/kalkulator/advCalc.java
@@ -6,24 +6,30 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class advCalc extends AppCompatActivity {
+    private String aktualneStan = "";
+    private double aktualneWynik = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adv_calc);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    private void updateScreen() {
+        TextView ekran = (TextView) findViewById(R.id.Wyswietlacz);
+        ekran.setText(this.aktualneStan);
+    }
+    public void addNumbAdv(View view){
+        if(aktualneStan == "0"){
+            aktualneStan = "";
+        }
+        aktualneStan += ((Button)view).getText().toString();
+        this.updateScreen();
+    }
 }
+
+
